26510ec27cf899d4c17f75c04aa0ad6ce90b796d
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2023-11-21 16:01:21 -0500

    Migrate storybook to 7.5.3

--- packages/ui-components/.storybook/main.js
+++ packages/ui-components/.storybook/main.js
@@ -1,13 +1,20 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
-  framework: '@storybook/react',
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+
   layout: 'centered',
-  core: {
-    builder: 'webpack5',
+
+  docs: {
+    autodocs: true,
   },
 };
