720629797623bf309086ff357e57c491e9fe50da
Marc Bernard <marc@marcbernardtools.com>
2026-03-27 19:23:20 +0100

    chore: express debug

--- packages/server/express/src/debug/index.ts
+++ packages/server/express/src/debug/index.ts
@@ -1,8 +1,10 @@
 import type { Application } from 'express';
 
+import type { Config as IConfig } from '@verdaccio/types';
+
 import type { $NextFunctionVer, $RequestExtend, $ResponseExtend } from '../../types/custom';
 
-export default (app: Application, configPath?: string): void => {
+export default (app: Application, config: IConfig): void => {
   // Hook for tests only
   app.get(
     '/-/_debug',
@@ -11,11 +13,25 @@ export default (app: Application, configPath?: string): void => {
         global.gc();
       }
 
+      // mask env DATABASE_URL
+      const env = { ...process.env };
+      env.DATABASE_URL = '********';
+      env.DATABASE_SECRET = '********';
+      env.DB_SALT = '********';
+      const sortedEnv: Record<string, string | undefined> = {};
+      Object.keys(env)
+        .sort()
+        .forEach((key) => {
+          sortedEnv[key] = env[key];
+        });
+
       next({
         pid: process.pid,
         // @ts-ignore
         main: process.main,
-        conf: configPath,
+        config,
+        env: sortedEnv,
+        uptime: process.uptime(),
         mem: process.memoryUsage(),
         gc: global.gc,
       });
