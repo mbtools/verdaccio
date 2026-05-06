2bd33db8ea4cd08a50355497cb09199a063c7a53
Marc Bernard <marc@marcbernardtools.com>
2026-05-06 15:27:38 +0200

    fix: esm loader

--- packages/loaders/test/partials/test-plugin-storage/verdaccio-plugin/index.js
+++ packages/loaders/test/partials/test-plugin-storage/verdaccio-plugin/index.js
@@ -2,7 +2,7 @@ class ValidVerdaccioPlugin {
   config;
   options;
   constructor(config, options) {
-    console.log('ValidVerdaccioPlugin constructor', config);
+    // console.log('ValidVerdaccioPlugin constructor', config);
     this.config = config;
     this.options = options;
   }
