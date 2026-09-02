d8ebfb2b5ca369dc2d546ce32f63b44c1044aa60
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2026-09-02 15:39:54 -0400

    chore: update apm plugins
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- abappm/plugins/@verdaccio-pro/middleware/build/middlewares/generate-sitemap.d.ts
+++ abappm/plugins/@verdaccio-pro/middleware/build/middlewares/generate-sitemap.d.ts
@@ -6,9 +6,5 @@ type SitemapStorage = {
         getStoragePlugin?: () => SitemapStorage;
     };
 };
-declare function getBaseUrl(req: Request): string;
-declare function buildPackageUrl(packageName: string): string;
-declare function buildSitemapXml(baseUrl: string, packageNames: string[]): string;
 declare const generateSitemap: (storage: SitemapStorage, logger: Logger) => (req: Request, res: Response) => Promise<void>;
-export { buildPackageUrl, buildSitemapXml, getBaseUrl };
 export default generateSitemap;
