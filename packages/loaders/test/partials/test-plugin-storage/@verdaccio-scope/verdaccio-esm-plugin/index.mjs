b191683c9d3a000391c2ccac66aeb9e1bb661eff
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2026-09-07 16:43:31 -0400

    chore(loader): remove duplicate tests

--- packages/loaders/test/partials/test-plugin-storage/@verdaccio-scope/verdaccio-esm-plugin/index.mjs
+++
@@ -1,8 +0,0 @@
-export default class VerdaccioScopedEsmAuthPlugin {
-  constructor(config, options) {
-    this.config = config;
-    this.options = options;
-  }
-
-  authenticate() {}
-}
--- packages/loaders/test/partials/test-plugin-storage/verdaccio-esm-plugin-store/index.mjs
+++
@@ -1,8 +0,0 @@
-export default class VerdaccioEsmStorePlugin {
-  constructor(config, options) {
-    this.config = config;
-    this.options = options;
-  }
-
-  getPackageStorage() {}
-}
